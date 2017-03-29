2300ade2d26cd0c0ab1fb11da3cec6c0265d8057
infle <infle@DESKTOP-MT07K1D>
2017-03-29 14:20:29 +0500

    Add project files.

---
+++ IslamicUloom/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IslamicUloom.MvcApplication" Language="C#" %>
---
+++ IslamicUloom/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IslamicUloom.Startup))]
+namespace IslamicUloom
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
